204eb54d043abcbe172e7a5dac123c2fca561c79
Blender20 <rayyazdi@outlook.com>
2022-05-05 21:11:41 -0400

    prop it up

--- prop_it_up/src/App.js
+++ prop_it_up/src/App.js
@@ -8,7 +8,8 @@ function App() {
       <PersonCard lastName = {"Doe"} name ={"Jane"} age = {45} hair_color = {"Black"}></PersonCard>
       <PersonCard lastName = {"Smith"} name ={"John"} age = {88} hair_color = {"Brown"}></PersonCard>
       <PersonCard lastName = {"Fillmore"} name ={"Millard"} age = {50} hair_color = {"Brown"}></PersonCard>
-      <PersonCard lastName = {"Smith"} name ={"Maria"} age = {62} hair_color = {"Brown"}></PersonCard>
+      <PersonCard lastName = {"Smith"} name ={"Maria"} age = {62} hair_color = {"Brown
+      "}></PersonCard>
       
 
     </div>
